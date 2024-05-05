47b7626d31a49d66832f3d79379ccdee0729f678
jihanlee <jihan.lee@emory.edu>
2024-05-04 22:36:36 -0400

    Update preprocessing.py

--- analysis/preprocessing.py
+++ analysis/preprocessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
@@ -10,14 +11,6 @@ def load_data(filepath="../data/2022_public_lar.csv"):
 
 def drop_cols(df, cols):
     return df.drop(columns=cols)
-
-
-def impute_numeric_cols(df, cols, strategy='median'):
-    imputer = SimpleImputer(strategy=strategy)
-    for col in cols:
-        df[col] = pd.to_numeric(df[col], errors='coerce')
-        df[col] = imputer.fit_transform(df[[col]]).ravel()
-    return df
 
 
 def recode_categorical_cols(df, recode_map):
@@ -38,64 +31,88 @@ def preprocess(df):
     df = drop_cols(df, redundant)
     df = drop_cols(df, sparse)
 
-    # Drop NAs
-    df = df.dropna()
-
-    # Drop observations with useless values (e.g., Not Available, Free From Text Only)
-    df = df[(df['derived_ethnicity'] != 'Ethnicity Not Available')
-            & (df['derived_ethnicity'] != 'Free Form Text Only')]
-    df = df[(df['derived_race'] != 'Race Not Available') &
-            (df['derived_race'] != 'Free Form Text Only')]
-    df = df[df['derived_sex'] != 'Sex Not Available']
+    # Drop observations with not useful values (e.g., Not Available, Free From Text Only)
+    df = df[(df['derived_ethnicity'] != 'Ethnicity Not Available') & (
+        df['derived_ethnicity'] != 'Free Form Text Only') & (df['derived_ethnicity'] != 'Joint')]
+    df = df[(df['derived_race'] != 'Race Not Available') & (
+        df['derived_race'] != 'Free Form Text Only') & (df['derived_race'] != 'Joint')]
+    df = df[(df['derived_sex'] != 'Sex Not Available')
+            & (df['derived_sex'] != 'Joint')]
     df = df[df['applicant_age'] != '8888']
 
     # Recode the target variable
-    df = df[df['action_taken'].isin([1, 2, 3, 7, 8])]
+    df = df[df['action_taken'].isin([1, 3])]
     df['loan_approved'] = df['action_taken'].apply(
-        lambda x: 1 if x in [1, 2, 8] else 0)
+        lambda x: 1 if x == 1 else 0)
     df = drop_cols(df, ['action_taken'])
 
-    # Impute numeric columns
-    numeric_cols = ['total_loan_costs', 'origination_charges', 'loan_term', 'property_value',
-                    'combined_loan_to_value_ratio', 'interest_rate', 'rate_spread']
-    df = impute_numeric_cols(df, numeric_cols)
+    # Change data types of some columns to numeric
+    numeric_cols = ['combined_loan_to_value_ratio', 'interest_rate', 'rate_spread',
+                    'origination_charges', 'loan_term', 'property_value', 'total_units', 'total_loan_costs']
+    for col in numeric_cols:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+
+    # Impute missing values with median for floating point columns and most frequent values (mode) for others
+    imputer_mode = SimpleImputer(strategy='most_frequent')
+    imputer_median = SimpleImputer(strategy='median')
+    for col in df.columns:
+        if df[col].dtype == 'float64':
+            df[col] = imputer_median.fit_transform(df[[col]]).ravel()
+        else:
+            df[col] = imputer_mode.fit_transform(df[[col]]).ravel()
 
     # Rename certain columns and typecasting
     rename_map = {'derived_msa_md': 'msa_md', 'derived_loan_product_type': 'loan_product_type', 'derived_dwelling_category': 'dwelling_category',
                   'derived_ethnicity': 'ethnicity', 'derived_race': 'race', 'derived_sex': 'sex', 'applicant_age': 'age'}
     df = df.rename(columns=rename_map).astype(
-        {'msa_md': object, 'loan_term': int, 'total_units': int, 'loan_approved': object})
+        {'msa_md': object, 'loan_term': int, 'total_units': int})
+
+    # Drop highly correlated features (preserve features which have higher correlation with the target)
+    corr = df.corr()
+    target_corr = corr.iloc[:-1, -1].abs()
+    col_to_drop = set()
+
+    for i in range(len(corr.columns) - 1):
+        for j in range(i+1, len(corr.columns) - 1):
+            if abs(corr.iloc[i, j]) > 0.9:
+                if target_corr[i] > target_corr[j]:
+                    col_to_drop.add(corr.columns[j])
+                else:
+                    col_to_drop.add(corr.columns[i])
+
+    df = df.drop(columns=list(col_to_drop))
+
+    df['loan_approved'] = df['loan_approved'].astype(object)
+
+    # Recode protected attributes to Privileged: 1, Unprivileged: 0
+    df['race'] = df['race'].apply(lambda x: 1 if x == 'White' else 0)
+    df['ethnicity'] = df['ethnicity'].apply(
+        lambda x: 1 if x == 'Not Hispanic or Latino' else 0)
+    df['sex'] = df['sex'].apply(lambda x: 1 if x == 'Male' else 0)
+    df['age'] = df['age'].apply(lambda x: 0 if x in ['<25', '>74'] else 1)
 
     # Replace values before one-hot encoding
     recode_map = {
-        'ethnicity': {'Not Hispanic or Latino': 'Non-Hispanic', 'Hispanic or Latino': 'Hispanic'},
-        'race': {'Black or African American': 'Black', 'American Indian or Alaska Native': 'Native',
-                 'Native Hawaiian or Other Pacific Islander': 'PI', '2 or more minority races': '2+Minority'},
         'preapproval': {2: 0},
         'loan_purpose': {1: 'Purchase', 2: 'Improvement', 31: 'Refinancing', 32: 'Cash-out', 4: 'Other', 5: 'N/a'},
-        'open_end_line_of_credit': {1: 'Yes', 2: 'No', 1111: 'Exempt'},
-        'business_or_commercial_purpose': {1: 'Yes', 2: 'No', 1111: 'Exempt'},
         'hoepa_status': {1: 'Yes', 2: 'No', 3: 'N/a'},
-        'negative_amortization': {1: 'Yes', 2: 'No', 1111: 'Exempt'},
-        'interest_only_payment': {1: 'Yes', 2: 'No', 1111: 'Exempt'},
-        'balloon_payment': {1: 'Yes', 2: 'No', 1111: 'Exempt'},
-        'other_nonamortizing_features': {1: 'Yes', 2: 'No', 1111: 'Exempt'},
         'occupancy_type': {1: 'Principal', 2: 'Second', 3: 'Investment'},
-        'manufactured_home_secured_property_type': {1: 'Home&Land', 2: 'Home', 3: 'N/a', 1111: 'Exempt'},
         'manufactured_home_land_property_interest': {1: 'Direct', 2: 'Indirect', 3: 'Paid', 4: 'Unpaid', 5: 'N/a', 1111: 'Exempt'},
-        'debt_to_income_ratio': {39.0: '39', 38.0: '38', 37.0: '37'}
+        'debt_to_income_ratio': {'39.0': '39', '38.0': '38'}
     }
     df = recode_categorical_cols(df, recode_map)
     df['preapproval'] = df['preapproval'].astype(object)
 
-    # Re-scaling numeric columns
+    # Re-scaling
     scaler = StandardScaler()
-    numeric_cols = df.select_dtypes(include=['number'])
-    df[numeric_cols.columns] = scaler.fit_transform(numeric_cols)
+    numeric_cols = ['loan_amount', 'combined_loan_to_value_ratio', 'interest_rate', 'rate_spread',
+                    'total_loan_costs', 'origination_charges', 'loan_term', 'property_value', 'total_units', 'income']
+    for col in numeric_cols:
+        df[col] = scaler.fit_transform(df[[col]])
 
     # One-hot encoding
-    categorical_cols = list(range(0, 7)) + list(range(8, 11)) + [15] + list(
-        range(19, 23)) + list(range(24, 27)) + list(range(29, 31))
+    df['msa_md'] = df['msa_md'].astype(object)
+    categorical_cols = list(range(0, 4)) + [8, 13, 18, 19, 22]
     encoded = pd.get_dummies(df.iloc[:, categorical_cols])
     df = df.reset_index(drop=True)
     encoded = encoded.reset_index(drop=True)
@@ -106,8 +123,8 @@ def preprocess(df):
     df.to_csv("../data/preprocessed_data.csv", index=False)
 
     # Separate features and target
-    y = df['loan_approved'].to_numpy().flatten()
-    X = df.drop('loan_approved', axis=1).to_numpy()
+    y = df['loan_approved']
+    X = df.drop('loan_approved', axis=1)
 
     return X, y
 
